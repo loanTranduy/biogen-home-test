1e35e334a92efea677e082fdaa4224242c81215a
loan tran duy <loan.tranduy@vandelvelde.eu>
2021-05-31 09:41:15 +0200

    typescript + folders setup

---
+++ src/constants/mocked-cards.tsx
@@ -0,0 +1,26 @@
+export const mockedCards = [
+        {
+          title: 'Neurogenium Aktuell',
+          score: 4.5,
+          tag: 'webinar',
+          follow: 7,
+        },
+        {
+          title: 'Experten-Interviews zur SMA',
+          score: 4.7,
+          tag: 'interviews',
+          follow: 5,
+        },
+        {
+          title: 'Nusinersen – Wirksamkeit bei 5q-SMA ist keine Frage des Alters?',
+          score: 4.1,
+          tag: 'symposium',
+          follow: 9,
+        },
+        {
+          title: 'Lesen und Lesen lassen',
+          score: 4.0,
+          tag: 'explainer',
+          follow: 3,
+        }
+];
---
+++ src/constants/mocked-video-previews.tsx
@@ -0,0 +1,32 @@
+export const mockedVideoPreviews = [
+    {
+      title: 'MS und Familienplanung Immuntherapien bei MS in ...',
+      completedTime: '2:42',
+      tag: 'episode 4',
+    },
+    {
+      title: 'MS und Familienplanung MS-Aktivität in der Schwangerschaft und danach',
+      completedTime: '1:30',
+      tag: 'episode 3',
+    },
+    {
+      title: 'Unter der Lupe – Können wir für Alzheimer von der MS lernen?',
+      completedTime: '0:46',
+      tag: 'episode 2',
+    },
+    {
+      title: 'Wechselwirkungen in der MS Therapie - Klinik und Pharmakologie',
+      completedTime: '3:23',
+      tag: 'episode 1',
+    },
+    {
+      title: 'Impfen bei MS zu Coronazeiten',
+      completedTime: '1:14',
+      tag: 'episode 5',
+    },
+    {
+      title: 'MS bei Kindern und im Alter',
+      completedTime: '0:42',
+      tag: 'episode 6',
+    },
+  ];
